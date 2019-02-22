29dd43e6eb9488039cdb774b5d22de5a80057549
Keanu Kerr <keanukerr@gmail.com>
2019-02-22 12:12:47 +0000

    Fixed UNK counts

--- plot.py
+++ plot.py
@@ -24,30 +24,39 @@ def getTextData(filename):
     types = set()
     lineLength = []
     tokens = 0
+    typeCounts = dict()
     with open(filename, 'r') as file:
         text = file.read()
         for line in text.splitlines():
             lineLength.append(len(line.split()))
             for word in line.split():
                 types.add(word)
-                tokens += 1  
-    return types, lineLength, tokens     
+                tokens += 1
+                if word not in typeCounts:
+                    typeCounts[word] = 1
+                else:
+                    typeCounts[word] += 1
+    
+    unks = [k for k,v in typeCounts.items() if v == 1]
+    unks = len(unks)
+
+    return types, lineLength, tokens, unks     
 
 
-types, lineLengths, tokens = getTextData(sys.argv[1])
+types, lineLengths, tokens, unks = getTextData(sys.argv[1])
 
 print("Total tokens in ", sys.argv[1], ": ", tokens)
 print("Total word types in ", sys.argv[1], ": ", len(types))
-print("UNK values in ", sys.argv[1], ": ")
+print("UNK values in ", sys.argv[1], ": ", unks)
 
 plotLengths(lineLengths)
 
 if(len(sys.argv) == 3):
-    types2, lineLengths2, tokens2 = getTextData(sys.argv[2])
+    types2, lineLengths2, tokens2, unks2 = getTextData(sys.argv[2])
 
-    print("Total tokens in ", sys.argv[2], ": ", tokens)
-    print("Total word types in ", sys.argv[2], ": ", len(types))
-    print("UNK values in ", sys.argv[2], ": ")
+    print("Total tokens in ", sys.argv[2], ": ", tokens2)
+    print("Total word types in ", sys.argv[2], ": ", len(types2))
+    print("UNK values in ", sys.argv[2], ": ", unks2)
 
     plotLengths(lineLengths2)
     plotCorrelation(lineLengths, lineLengths2)
